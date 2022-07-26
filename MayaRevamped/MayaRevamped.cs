8c8913def325278319ab17d468bc2a553bb5641e
FazyDota <Fazy64+dota@gmail.com>
2022-07-26 08:09:12 +0200

    Cleanup for initial release

--- MayaRevamped/MayaRevamped.cs
+++ MayaRevamped/MayaRevamped.cs
@@ -15,34 +15,33 @@ public class MayaRevamped : Mod
     public static Texture2D originalMayaDiffuseTexture;
     public static Texture2D originalMayaNormalTexture;
 
-    public static AssetBundle mayaBundle;
-    public static Texture2D loadedMayaDiffuseTexture;
-    public static Texture2D loadedMayaNormalTexture;
-    public static byte[] loadedAndroMayaVerticeBytes;
-    public static Vector3[] loadedAndroMayaVertices;
+    // Loaded resources
+    public static AssetBundle androMayaBundle;
+    public static Texture2D androMayaDiffuseTexture;
+    public static Texture2D androMayaNormalTexture;
+    public static byte[] androMayaVerticeBytes;
+    public static Vector3[] androMayaVertices;
 
-    public static int mode = 0;
-    public static bool initialized = false;
+    public static bool defaultResourcesInitialized = false;
 
     public IEnumerator Start()
     {
         // Get external resources - vertices from JSON, updated normal map texture and updated diffuse texture
         AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(GetEmbeddedFileBytes("mayabundle"));
         yield return request;
-        mayaBundle = request.assetBundle;
+        androMayaBundle = request.assetBundle;
 
-        loadedAndroMayaVerticeBytes = GetEmbeddedFileBytes("AndroMayaVertices.json");
+        androMayaVerticeBytes = GetEmbeddedFileBytes("AndroMayaVertices.json");
 
-        MemoryStream verticesStream = new MemoryStream(loadedAndroMayaVerticeBytes);
+        MemoryStream verticesStream = new MemoryStream(androMayaVerticeBytes);
         using (StreamReader verticesReader = new StreamReader(verticesStream, Encoding.UTF8))
         {
             JsonSerializer serializer = new JsonSerializer();
-            loadedAndroMayaVertices = (Vector3[])serializer.Deserialize(verticesReader, typeof(Vector3[]));
-            Debug.Log("loadedAndroMayaVertices");
+            androMayaVertices = (Vector3[])serializer.Deserialize(verticesReader, typeof(Vector3[]));
         }
 
-        loadedMayaNormalTexture = mayaBundle.LoadAsset<Texture2D>("updatedNormalMapV4");
-        loadedMayaDiffuseTexture = mayaBundle.LoadAsset<Texture2D>("betterTextureV2");
+        androMayaNormalTexture = androMayaBundle.LoadAsset<Texture2D>("updatedNormalMapV4");
+        androMayaDiffuseTexture = androMayaBundle.LoadAsset<Texture2D>("betterTextureV2");
 
         Debug.Log("Mod MayaRevamped has been loaded! What a wonderful day.");
     }
@@ -57,7 +56,7 @@ public class MayaRevamped : Mod
 
     public static void loadPlayerRelated()
     {
-        if (initialized) return;
+        if (defaultResourcesInitialized) return;
         localPlayer = RAPI.GetLocalPlayer();
         
         Texture currentDiffuseTexture = localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].GetTexture("_Diffuse");
@@ -66,20 +65,20 @@ public class MayaRevamped : Mod
         originalMayaNormalTexture = (Texture2D)currentNormalTexture;
 
         originalMayaVertices = localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices;
-        initialized = true;
+        defaultResourcesInitialized = true;
     }
 
     public static void setAndroMaya()
     {
-        if (!initialized) loadPlayerRelated();
+        if (!defaultResourcesInitialized) loadPlayerRelated();
         localPlayer = RAPI.GetLocalPlayer();
         VoiceType currentVoiceType = localPlayer.currentModel.voiceType;
         if (currentVoiceType == VoiceType.Maya)
         {
             Debug.Log("VoiceType is Maya.");
-            localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = loadedAndroMayaVertices;
-            localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", loadedMayaDiffuseTexture);
-            localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", loadedMayaNormalTexture);
+            localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = androMayaVertices;
+            localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", androMayaDiffuseTexture);
+            localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", androMayaNormalTexture);
             Debug.Log("Swapped vertices, diffuse and normal texture to AndroMaya successfully.");
         }
         else
@@ -88,84 +87,42 @@ public class MayaRevamped : Mod
         }
     }
 
+    public static int switchCommandMode = 1;
+
     [ConsoleCommand(name: "switchMayaVisual", docs: "Swaps back and forth between different Maya models and textures.")]
     public static string SwitchMayaVisual(string[] args)
     {
-        if (!initialized) loadPlayerRelated();
-        switchMaya();
-        return $"Command switchMayaVisual finished.";
-    }
-
-    public static void switchMaya()
-    {
+        if (!defaultResourcesInitialized) loadPlayerRelated();
         Raft_Network raftNetwork = ComponentManager<Raft_Network>.Value;
         if (localPlayer is null)
         {
             localPlayer = RAPI.GetLocalPlayer();
 
         }
-        var currentVoiceType = raftNetwork.GetLocalPlayer().currentModel.voiceType;
-        Debug.Log($"Current voice type: {currentVoiceType}");
-        Debug.Log($"Current voice type: {currentVoiceType.ToString()}");
-        Debug.Log($"Current voice type: {((int)currentVoiceType)}");
-        Debug.Log($"Current remote users: {raftNetwork.remoteUsers}");
-
-        foreach (var user in raftNetwork.remoteUsers)
-        {
-            Debug.Log(user.Key, user.Value);
-            Debug.Log(user.ToString());
-        }
-
-        Network_Player currentNetworkPlayer = raftNetwork.GetLocalPlayer();
+        var currentVoiceType = localPlayer.currentModel.voiceType;
 
         if (currentVoiceType == VoiceType.Maya)
         {
-            Debug.Log($"YES, MAYA CHARACTER IN USE!");
-            if (mode == 0)
+            if (switchCommandMode == 0)
             {
                 localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = originalMayaVertices;
                 localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", originalMayaDiffuseTexture);
                 localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", originalMayaNormalTexture);
 
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = originalMayaVertices;
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", originalMayaDiffuseTexture);
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", originalMayaNormalTexture);
-
-                Debug.Log("0: DEFAULT");
-                mode = 1;
-                return;
+                switchCommandMode = 1;
+                return $"Swapping back to default Maya vertices and textures.";
             }
 
-            if (mode == 1)
+            if (switchCommandMode == 1)
             {
-                localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = loadedAndroMayaVertices;
-                localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", loadedMayaDiffuseTexture);
-                localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", loadedMayaNormalTexture);
+                localPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = androMayaVertices;
+                localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", androMayaDiffuseTexture);
+                localPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", androMayaNormalTexture);
 
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMesh.vertices = loadedAndroMayaVertices;
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Diffuse", loadedMayaDiffuseTexture);
-                currentNetworkPlayer.currentModel.fullBodyMesh.sharedMaterials[0].SetTexture("_Normal", loadedMayaNormalTexture);
-
-                Debug.Log("3: IMPROVED VERTICES B AND DIFFUSE, NORMALMAP B");
-                mode = 0;
-                return;
+                switchCommandMode = 0;
+                return $"Swapping to edited Maya vertices and textures.";
             }
         }
-        else
-        {
-            Debug.Log($"NO, OTHER CHARACTER IN USE!");
-        }
-        return;
-    }
-
-    // currently not used, todo 
-    [HarmonyPatch(typeof(CharacterModelModifications), "Start")]
-    public class HarmonyPatch_IgnoreCollisionOnAlt
-    {
-        [HarmonyPostfix]
-        static void SwapModelIfMaya()
-        {
-            //setAndroMaya();
-        }
+        return $"Not using Maya character.";
     }
 }
